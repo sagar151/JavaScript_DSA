f5fbb74ecf2a54e0d93c176808dc48c321a6024c
Sagar Gajera <sagargajera151@gmail.com>
2024-02-14 00:31:29 +0530

    Day 2 Move all negative to end

---
+++ Day2_move_all_negative_to_end.js
@@ -0,0 +1,24 @@
+const moveNegativeToEnd=(arr)=>{
+    let n=arr.length;
+   let newArr=new Array(n);
+   let counter=0
+   for(let i=0;i<n;i++){
+       if(arr[i]>=0){
+           newArr[counter++]=arr[i]
+       }
+   }
+   if (counter===n || counter===0){
+       return;
+   }
+    for(let i=0;i<n;i++){
+        if(arr[i]<0){
+            newArr[counter++]=arr[i]
+        }
+    }
+    for(let i=0;i<n;i++){
+      arr[i]=newArr[i]
+    }
+
+   return arr;
+}
+console.log(moveNegativeToEnd([1, -1, 3, 2, -7, -5, 11, 6 ]))
